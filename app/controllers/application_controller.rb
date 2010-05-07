1b03fe876c537ea760ed572a166dad94b0b8688b
chlu <hlubek@networkteam.com>
2010-05-08 00:02:41 +0200

    Always authorize contoyo backend (until some authentication is implemented), configuration of layout (not used yet)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
+  before_filter :authorize_contoyo
+
+  def authorize_contoyo
+    Contoyo.authorized = true
+  end
 end
--- config/initializers/contoyo.rb
+++ config/initializers/contoyo.rb
@@ -1,3 +1,8 @@
 Contoyo.configure do |config|
   # config.add_content_type
+
+  config.layouts.merge! 'application' => {
+          :name => 'Application layout with sidebar',
+          :containers => ['sidebar']
+        }
 end
